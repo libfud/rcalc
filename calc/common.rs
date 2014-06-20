286d1e6f97a3e8a8c7bc6e2fe3a44a5e25359993
libfud <libfud@librehumanitas.org>
2014-06-20 09:09:12 -0400

    testing something out

--- calc/common.rs
+++ calc/common.rs
@@ -2,10 +2,12 @@
 
 extern crate num;
 
+
 use super::{Evaluate, Environment, CalcResult};
 use super::literal::{ Void};
 use self::num::rational::{BigRational, Ratio};
 use std::num;
+use std::collections::hashmap::HashMap;
 
 static PI: &'static str = "3126535/995207";
 
@@ -25,11 +27,11 @@ It requires that expressions are wrapped in parentheses ( \"( and \") ).
 The operator goes first in any expression, followed by its terms.
 For an  example, the following returns -3/2:
 
-(/ (- 6 (pow 9 (/ 2)) (+ -4.5 13/2)))
+(/ (- 6 (pow 9 (/ 2)) (+ -4.5 13/2))) ; 2/3
 
 As you can see, you can nest expressions. Not all expressions are evaluated:
 
-(+ 2 (if (> 3 2) (+ 7 3) (* 2)))
+(+ 2 (if (> 3 2) (+ 7 3) (* 2))) ; 12
 
 only (> 3 2) and (+ 7 3) are evaluated in that case, and then added to 2.
 
@@ -42,72 +44,59 @@ want to express the reciprocal of a number, either input it as 1/n, where n is
 a numeric literal, or as (/ n). Input as numbers with an explicit radix 
 is quite buggy due to the limitations of IEEE754.";
 
-    let abs_help =
-"The absolute function. Takes only one term, and returns its absolute value.
-
-(abs -5.5) -> 11/2
-(abs 17/7) -> 17/7";
-    
     let arithmetic_help =
 "The arithmetic operators are +, -, *, /, %, and pow.
 In general, the arithmetic operators can take 0 terms, or as many terms as you
 like. For example, pow acts like a tower of power.
 
-(pow 2 3 4)
+(pow 2 3 4) ; 2417851639229258349412352
 
 is equivalent to (pow 2 81).
 To see more help, use help with the appropriate arithmetic operator.
 Aadditionally, see (help abs), as I have not classified it yet.";
 
-    let stats_help =
-"Statistical functions. Currently only comprised of avg, which returns the
-arithmetic mean of a list of terms.
-
-(avg 42) -> 42/1
-(avg 7.5 12 -13.25 4/5) -> 161/80";
-
     let add_help =
 "The addition operator. If no terms are supplied, returns the the additive
 identity, 0. The examples below are valid expressions:
 
-(+ ) -> 0/1
-(+ 7) -> 7/1
-(+ 2 2) -> 4/1
-(+ 3 4 (/ 25 5) 6) -> 18/1
-(+ -3.5) -> -7/2 ";
+(+ ) ; 0
+(+ 7) ; 7
+(+ 2 2) ; 4
+(+ 3 4 (/ 25 5) 6) ; 18
+(+ -3.5) ; -7/2 ";
 
     let sub_help = 
 "The subtraction operator. Requires at least one term. If only one term is
 given, it returns the negation of that term. The following are examples of
 valid expressions:
 
-(- 2) -> -2/1
-(- -1.5) -> 3/2
-(- 3 2) -> 1/1
-(- 10 3 3) -> 4/1
-(- (+ 2 2) 1) -> 3/1";
+(- 2) ; -2
+(- -1.5) ; 3/2
+(- 3 2) ; 1
+(- 10 3 3) ; 4
+(- (+ 2 2) 1) ; 3/";
 
     let mul_help =
 "The multiplication operator. If zero terms are given, returns the
 multiplicative identity, one. The following are examples of valid
 expressions:
 
-(* ) -> 1/1
-(* -2/3 -3/2) -> 1/1
-(* 2 3 4) -> 24/1
-(* .5 .25) -> 1/8
-(* 0.75 100) -> 750";
+(* ) ; 1
+(* -2/3 -3/2) ; 1
+(* 2 3 4) ; 24
+(* .5 .25) ; 1/8
+(* 3/4 100) ; 75";
 
     let div_help =
 "The division operator. Requires at least one term. If only one term is
 given, Division by zero is undefined. it returns that term's reciprocal.
 Valid examples of expressions:
 
-(/ 2) -> 1/2
-(/ 4/5 -1.25) -> -16/25
-(/ 24 6 2) -> 2
-(/ (pow 2 2 2) 4) -> 4
-(/ 144 12 4 3) -> 1";
+(/ 2) ; 1/2
+(/ 4/5 -1.25) ; -16/25
+(/ 24 6 2) ; 2
+(/ (pow 2 2 2) 4) ; 4
+(/ 144 12 4 3) ; 1";
 
     let pow_help =
 "The exponentiation operator. If supplied no arguments, returns the
@@ -115,26 +104,14 @@ multiplcative identity, 1. If only one term is supplied, the implied power is
 1. Exponentiation with zero as a base is allowed, but has some notable
 behavior:
 
-(pow 0) -> 0/1     | 0 to any non zero power is 0.
-(pow 0 0) -> 1/1   | 0^0 is one.
-(pow 0 0 0) -> 0/1 | Problem?
-
-If you are asking what's going on with 0, it's because pow is evaluated
-recursively from left to right: if more than two terms are supplied, it
-essentially evaluates from the rightmost two terms, treating the very
-last as the power and the second to last as the base.
-
-(pow 0 0 0) -> (pow 0 (pow 0 0)) -> (pow 0 1) -> 0/1
-
-That's all I have to say on that matter. Below are valid expressions:
-
-(pow 2 2 2 2) -> 65536/1
-(pow 2 3 4)   -> 24178851639229258349412352/1
-(pow 2 .5)    -> 577/408
-(pow 27 1/3)  -> 3/1
-(pow 256 1/8) -> 2/1
-(pow 2 -1)    -> 1/2
-(pow 8 -2)    -> 1/64";
+(pow 0)   ; 0 to any non zero power is 0.
+(pow 0 0) ; 1  0^0 is one.
+(pow 0 0 0) ; 0  Problem?
+(pow 2 2 2 2)  ; 65536
+(pow 2 .5)    ; 577/408
+(pow 256 1/8) ; 2
+(pow 2 -1)    ; 1/2
+(pow 8 -2)    ; 1/64";
 
     let trig_help =
 "Trigonometric functions. Currently only comprised of sin, cos, and tan. 
@@ -146,91 +123,83 @@ a single term.";
 zero, which is still zero. Uses radians, not degrees. If you want to
 express the angle in degrees, convert with (* degrees pi (/ 180)).
 
-(sin pi)    -> 0
-(sin (* 1/2 pi)) -> 1
-(sin (/ (* 2 pi) 3)) -> 0.866025";
+(sin pi)  ; 0
+(sin (* 1/2 pi)) ; 1
+(sin (/ (* 2 pi) 3)) ; 7800459734587069/9007199254740992";
 
     let cos_help =
 "The cosine function. Takes one term. If no terms are supplied, it evaluates
 zero. Uss radians. If you want to express the angle in degrees, convert with
 the formula (* degrees pi (/ 180))
 
-(cod ) -> 1/1
-(cos pi) -> -1";
+(cos 0) ; 1
+(cos pi) ; -1";
 
     let tan_help =
 "The tangent function. Takes exactly one term.
 
-(tan 1) -> 1.55
-(tan pi) -> 0
-(tan 0) -> 0";
-
-    let avg_help =
-"Returns the arithmetic mean of a set of numbers. Requires at least one term.
-
-(avg 2 3 4) -> 3/1
-(avg 12.25 -7/3 -14) -> -49/36
-(avg 42 6 7 13 9 9 9) -> 95/7";
+(tan pi) ; 0
+(tan 0) ; 0";
 
     let logic_help =
 "The ordering operators are <, <=, =, >=, >. Additionally, you can compose
 conditional statements with if. And, Or, and Not are also availalbe.
 
-(> 3 2) -> true
-(= 7 3) -> false
-(if (> 3 2) (+ 7 3) (/ 10 2)) -> 10/1";
+(> 3 2) ; true
+(= 7 3) ; false
+(if (> 3 2) (+ 7 3) (/ 10 2)) ; 10";
 
     let condit_help =
 "The if statement takes three expressions as arguments: a conditional
 statement, a consequent, and an alternative.
 
-(if (> 3 2) (+ 7 3) (/ 10 2)) -> 10/1
-(+ 2 (if (= (pow 2 2 2) (abs -16)) (+ 1 1) (* pi 2)) 2) -> 6/1";
+(if (> 3 2) (+ 7 3) (/ 10 2)) ; 10
+(+ 2 (if (= (pow 2 2 2) (abs -16)) (+ 1 1) (* pi 2)) 2) ; 6";
 
     let lt_help =
-"The ordering operator, <, takes two terms, and returns either true or false.
-
-(< 3 2) -> true
-(< 3 pi) -> false";
+"The ordering operator, <, takes two terms, and returns either true or false.";
 
     let lte_help =
-"The ordering operator, <=, takes two terms, and returns either true or false.
-
-(<= 7 6) -> true
-(<= 6/3 3/2) -> false";
+"The ordering operator, <=, takes two terms, and returns either true or false.";
 
     let eq_help =
-"The ordering operator, =, takes two terms, and returns either true or false.
-
-(= 13 13) -> true
-(= pi e) -> false";
+"The ordering operator, =, takes two terms, and returns either true or false.";
 
     let gte_help =
-"The ordering operator, >=, takes two terms, and returns either true or false.
-
-(>= 7 6) -> true
-(>= pi 13/2) -> false";
+"The ordering operator, >=, takes two terms, and returns either true or false.";
 
     let gt_help =
-"The ordering operator, >, takes two terms, and returns either true or false.
-
-(> 9 5/4) -> true
-(> e pi) -> false";
+"The ordering operator, >, takes two terms, and returns either true or false.";
 
     let define_help = 
 "Define a variable. You can define it as another variable, or the result of a
 function.
 
-(define x 42)
-(define y (/ x 7))
-(define z (+ (* x y) x))";
+(define x 42) ; x = 42
+(define y (/ x 7)) ;  y = 6;
+(define z (+ (* x y) x)) ; z = 294
+(define (f x) (* x 2 (+ x 2))) ; (f 2) is 16, (f 3) is 30
+(define (h g x) (* (g x) 3 (+ (g x) 2))) ; (h f 4) is 7200";
 
-let defun_help = 
-"Define a function. Place the function between pipes.
+    let lambda_help =
+"The Anonymous function. (lambda (arguments) (body)).
+(reduce (lambda (x y) (+ x y)) 0 '(1 2 3 4 5)) ; 15
+(define (h g x) (* (g x) 3 (+ (g x) 2)))
+(h (lambda (x) (* x 7)) 4) ; The result is 1080";
 
-(defun |f (x) (+ x 2)|)
-(defun |g (x y) (* x y)|)
-(defun |h (x y z) (/ (* x (+ x 1) (* 2 (+ x 1))) 6)";
+    let mut help_map: HashMap<String, String> = HashMap::new();
+
+    for (key, val) in ["help", "use", "arithmetic", "+", "-", "*", "/", "pow", "sin",
+                       "cos", "tan", "trig", "<", "<=", "=", ">=", ">", "if", "logic",
+                       "define", "lambda"]
+        .iter().zip([help_help, use_help, arithmetic_help, add_help, sub_help, mul_help,
+                     div_help, pow_help, sin_help, cos_help, tan_help, trig_help,
+                     lt_help, lte_help, eq_help, gte_help, gt_help, condit_help, logic_help,
+                     define_help, lambda_help].iter())
+    {
+            help_map.insert(key.to_str(), val.to_str());
+    }
+                                                       
 
     let list: Vec<String> = args.iter().map(|x| x.to_symbol(env)).collect();
 
@@ -240,10 +209,14 @@ let defun_help =
     }
 
     for word in list.iter() {
+        println!("{}", match help_map.find(word) {
+            Some(val) => format!("{}", val),
+            None => format!("Help for `{}' is not available", word)
+        });
+/*
         println!("{}", match word.as_slice() {
             "help"              => help_help,
             "use"               => use_help,
-            "abs"               => abs_help,
             "arithmetic"        => arithmetic_help,
             "+"|"add"           => add_help,
             "-"|"subtraction"   => sub_help,
@@ -254,8 +227,6 @@ let defun_help =
             "cos"|"cosine"      => cos_help,
             "tan"|"tangent"     => tan_help,
             "trig"              => trig_help,
-            "stats"             => stats_help,
-            "avg"               => avg_help,
             "<"                 => lt_help,
             "<="                => lte_help,
             "="                 => eq_help,
@@ -264,10 +235,11 @@ let defun_help =
             "if"                => condit_help,
             "logic"             => logic_help,
             "define"            => define_help,
-            "defun"             => defun_help,
+            "lambda"            => lambda_help,
             _                   => "More help is not available at this time."
             }
         )
+*/
     }
 
     return Ok(Void)
--- calc/expression.rs
+++ calc/expression.rs
@@ -1,15 +1,21 @@
 //! Expressions
 
+extern crate num;
+
+use self::num::rational::BigRational;
+
 use super::{function, operator, CalcResult, Evaluate, Environment};
 use super::tokenize;
 use super::tokenize::Token;
 use super::operator::OperatorType;
+use super::literal::LiteralType;
 
 #[deriving(Show, Clone)]
 pub enum ExprType {
     Operator(OperatorType),
     Function(String)
 }
+
 
 pub fn token_to_expr(token: Token) -> Result<ExprType, String> {
     match token {
@@ -50,4 +56,53 @@ impl Evaluate for Expression {
     fn to_symbol(&self, _: &mut Environment) -> String {
         self.expr_type.to_str()
     }
+}
+
+#[deriving(Show, Clone)]
+pub struct SExpression {
+    pub expr_type: ExprType,
+    pub args: Vec<ArgType>,
+}
+
+impl SExpression {
+    fn new(e: ExprType, a: Vec<ArgType>) -> SExpression {
+        SExpression { expr_type: e, args: a }
+    }
+}
+
+impl Evaluate for SExpression {
+    fn eval(&self, _: &mut Environment) -> CalcResult {
+        Ok(super::literal::Void)
+    }
+
+    fn to_symbol(&self, _: &mut Environment) -> String {
+        self.expr_type.to_str()
+    }
+}
+
+#[deriving(Clone, Show)]
+pub enum ArgType {
+    Atom(BasicType),
+    SExpr(SExpression),
+}
+
+#[deriving(Clone, Show)]
+pub enum BasicType {
+    Boolean(bool),
+    BigNum(BigRational),
+    Symbol(String),
+    Proc(Vec<String>, SExpression),
+    List(Vec<LiteralType>),
+    Void
+}
+
+//Just seeing if this might be a valid path to walk down.
+fn test() {
+    let new_e = SExpression::new(Operator(super::operator::Add),
+                                 vec!(Atom(
+                                     BigNum(from_str::<BigRational>("1/1").
+                                                             unwrap()))));
+
+    let new_r = SExpression::new(Operator(super::operator::Add),
+                                 vec!(SExpr(new_e)));
 }
