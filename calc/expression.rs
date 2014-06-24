114726a29fb400696b3b4f0f02f9a0b4fd751083
libfud <libfud@librehumanitas.org>
2014-06-24 09:54:34 -0400

    comments

--- calc/expression.rs
+++ calc/expression.rs
@@ -33,8 +33,18 @@ impl Expression {
     pub fn eval(&self, env: &mut Environment) -> CalcResult {
         let mut ops_stack: Vec<ExprType> = Vec::new();
         let mut data: Vec<Vec<ArgType>> = vec!(self.args.clone());
-
         let mut holding_block: Vec<Vec<ArgType>> = Vec::new();
+
+        /* Iterate through the arguments. Initially, the very first vector,
+         * is a clone of `args', is popped as `arguments'. Then, an empty vector
+         * is pushed onto `data'. `arguments' is then iterated through. While the values
+         * in it are Atoms, those values are pushed back into the last vector in 
+         * `data'. When an `SExpr' is encountered, the expression type, Operator(op) or 
+         * Function(f), is pushed onto `ops_stack', the `args' of the type are pushed
+         * onto another vector in data, and the remaining values in `arguments' are 
+         * pushed onto `holding_block'.
+         */
+
         loop {
             let arguments = data.pop().unwrap();
             data.push(vec![]);
@@ -56,6 +66,13 @@ impl Expression {
                 }
                 arg_index += 1;
             }
+
+            /* If the ops_stack has entities on it, check to see if they're all Atoms.
+             * If so, we then pop the last vector in data, and then match the popped
+             * value from ops_stack, applying the appropriate function. The result
+             * is then pushed back onto the last vector of data, and then whatever
+             * the holding block has within it is pushed on as well.
+             */
             
             if ops_stack.len() > 0 {
                 if data.last().unwrap().iter().all(|x| match x {
@@ -70,9 +87,7 @@ impl Expression {
                     data.mut_last().unwrap().push(result);
                     data.mut_last().unwrap().push_all(holding_block.pop().unwrap().as_slice());
                 }
-            }
-
-            if ops_stack.len() == 0 {
+            } else {
                 break
             }                             
         }
