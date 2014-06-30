9694ce00239a276c1090485888e843bb8abe47c2
libfud <libfud@librehumanitas.org>
2014-06-30 10:38:09 -0400

    cleaned up operator a bit

--- calc/operator/mod.rs
+++ calc/operator/mod.rs
@@ -34,6 +34,7 @@ pub enum OperatorType {
     Help, Table, RangeList, Sort, 
 }
 
+type BR<T = BigRational> = T;
 impl OperatorType {
     pub fn to_numops(self) -> logic::NumOps {
         match self {
@@ -50,6 +51,24 @@ impl OperatorType {
             Eq => |a: T, b: T| a == b, NEq => |a: T, b: T| a != b,
             GtEq => |a: T, b: T| a >= b, Gt => |a: T, b: T| a > b,
             _ => fail!("Mismatched operator types (don't use ord with nonord)")
+        }
+    }
+
+    pub fn to_arith(&self) -> |BR, &BR| -> BR {
+        match self {
+            &Add => |a: BR, b: &BR| a + *b, &Sub => |a: BR, b: &BR| a - *b,
+            &Mul => |a: BR, b: &BR| a * *b, &Div => |a: BR, b: &BR| a / *b,
+            &Rem => |a: BR, b: &BR| a % *b,
+            _ => fail!("Mismatched operator types (don't use arith with non-arithmetic")
+        }
+    }
+
+    pub fn to_arith_args(&self) -> (uint, |BR, &BR| -> BR, || -> BR) {
+        match self {
+            &Add => (0, self.to_arith(), || num::zero()),
+            &Sub => (1, self.to_arith(), || num::zero()),
+            &Mul => (0, self.to_arith(), || num::one()),
+            _ => fail!("Don't try div or rem with this one.")
         }
     }
 }
@@ -122,59 +141,52 @@ pub fn to_str(op: &OperatorType) -> String {
 
 pub fn eval(op_type: OperatorType, args: &Vec<ArgType>, 
             env: &mut Environment) -> CalcResult {
+    use self::logic::{num_op, and_or, not, xor, ordering};
+    use self::arithmetic::{do_op, divrem};
+    use self::listops::{map, filter, reduce, rangelist, listlen};
+    use self::special::{table, sort};
+    use self::trig::float_ops;
+
     match op_type {
         Define  => super::define(args, env),
         Lambda => Ok(Atom(super::literal::Void)),
 
         List => list(args, env),
         Quote => Ok(Atom(super::literal::Void)),
-
-        Cons => cons(args, env), Car => car(args, env), Cdr => cdr(args, env),
-        Cadr => car(&vec!(try!(cdr(args, env))), env),
+        Cons => cons(args, env), 
+        Car => car(args, env), Cdr => cdr(args, env),
+        Cadr => car(&vec!(try!(cdr(args, env))), env), 
         Cddr => cdr(&vec!(try!(cdr(args, env))), env),
         Caddr => car(&vec!(try!(cdr(&vec!(try!(cdr(args, env))), env))), env),
         Cdddr => cdr(&vec!(try!(cdr(&vec!(try!(cdr(args, env))), env))), env),
 
-        Map => listops::map(args, env), Reduce => listops::reduce(args, env),
-        Filter => listops::filter(args, env),
+        Map => listops::map(args, env), Reduce => reduce(args, env),
+        Filter => filter(args, env),
 
-        Add => arithmetic::do_op(args, env, 0, |a, b| a + *b, num::zero),
-
-        Sub => arithmetic::do_op(args, env, 1, |a, b| a - *b, num::zero),
-
-        Mul => arithmetic::do_op(args, env, 0, |a, b| a * *b, num::one),
-
-        Div => arithmetic::divrem(args, env, |a, b| a / *b), 
-        //division can fail with zeros
-
-        Rem => arithmetic::divrem(args, env, |a, b| a % *b),
-
+        Add | Sub | Mul => {
+            let (min, op, ident) = op_type.to_arith_args();
+            do_op(args, env, min, op, ident)
+        },
+        Div | Rem  => divrem(args, env, op_type.to_arith()),
         Pow => power::pow_wrapper(args, env),
 
-        Sin | Cos | Tan | ASin | ACos | 
-        ATan | SinH | CosH | TanH | ASinH |
-        ACosH | ATanH | Log | Ln | Exp => trig::float_ops(args, env, op_type),
+        Sin | Cos | Tan | ASin |
+        ACos | ATan | SinH | CosH | TanH | 
+        ASinH | ACosH | ATanH | Log | Ln | Exp => float_ops(args, env, op_type),
 
         If   => logic::cond(args, env),
+        And  => and_or(args, env, false), Or => and_or(args, env, true),
+        Not  => not(args, env), Xor  => xor(args, env),
 
-        And  => logic::and_or(args, env, false),
+        Lt | LtEq | Eq | NEq | GtEq | Gt => ordering(args, env, op_type.to_ord()),
 
-        Or   => logic::and_or(args, env, true),
-
-        Not  => logic::not(args, env),
-
-        Xor  => logic::xor(args, env),
-
-        Lt | LtEq | Eq | NEq | GtEq | Gt => logic::ordering(args, env, op_type.to_ord()),
-
-        Round | Ceiling | Floor | 
-        Zero | Even | Odd => logic::num_op(args, env, op_type.to_numops()),
+        Round | Ceiling | Floor | Zero | Even | Odd => num_op(args, env, op_type.to_numops()),
 
         Help => super::common::help(args),
 
         Table => special::table(args, env),
 
-        RangeList => listops::rangelist(args, env), Sort => special::sort(args, env), 
-        ListLen => listops::listlen(args, env),
+        RangeList => rangelist(args, env), Sort => sort(args, env), 
+        ListLen => listlen(args, env),
     }
 }
