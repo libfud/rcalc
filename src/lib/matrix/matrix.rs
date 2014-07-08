3f55583fc31fb41e268d04dcb5a2fb47c7309ad1
libfud <libfud@librehumanitas.org>
2014-07-08 06:36:18 -0400

    trying to clean up matrix multiplication

--- src/lib/matrix/matrix.rs
+++ src/lib/matrix/matrix.rs
@@ -282,21 +282,24 @@ impl<T: Num + Clone> Mul<Matrice<T>, Matrice<T>> for Matrice<T> {
         if self.length != other.height {
             fail!(MismatchedAxes.to_str())
         }
-/*
-        let (a, b) = match (self.length == other.height, self.height == other.height) {
-            (true, _) => (self, other),
-            (false, true) => (other, self),
-            (false, false) => fail!(MismatchedAxes.to_str())
-        };
-*/
         let zero: T = num::zero();
 
-        let new_elems: Vec<T> = range(0, self.length).map(|x| {
-            let prods: Vec<T> = self.get_row(x).zip(other.get_col(x)).map(|(lhs, rhs)|
-                                                                   *lhs * *rhs).collect();
-            prods.iter().fold(zero.clone(), |a, b| a + *b)
-        }).collect();
+        let new_elems: Vec<T> = range(0, self.length)
+            .map(|row| range(0, self.length).map(|col| self.get_row(row).zip(other.get_col(col))
+                                                 .map(|(lhs, rhs)| *lhs * *rhs)
+                                                 .fold(zero.clone(), |a, b| a + b))).collect();
 
+/*
+        let mut new_elems: Vec<T> = Vec::with_capacity(self.length * other.height);
+        for row in range(0, self.length) {
+            for col in range(0, self.length) {
+                let prods: Vec<T> = self.get_row(row).zip(other.get_col(col))
+                    .map(|(lhs, rhs)| *lhs * *rhs).collect();
+
+                new_elems.push(prods.iter().fold(zero.clone(), |a, b| a + *b));
+            }
+        }
+         */              
         Matrice { length: self.length, height: other.height, elems: new_elems }
                 
     }
