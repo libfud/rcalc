b66186ed5e641eecf94e822abaedc53b1d58aea3
libfud <libfud@librehumanitas.org>
2014-07-23 07:54:57 -0400

    trying to use RC

--- src/lib/types/types.rs
+++ src/lib/types/types.rs
@@ -11,7 +11,8 @@ pub use sexpr::{ArgType, Atom, SExpr, Expression};
 pub use operator::OperatorType;
 use std::collections::hashmap::HashMap;
 use std::fmt;
-use std::rc::Rc;
+use std::rc;
+use std::cell::RefCell;
 
 pub mod sexpr;
 pub mod literal;
@@ -62,29 +63,48 @@ pub type CalcResult<T = ArgType> = Result<T, ErrorKind>;
 #[deriving(Clone)]
 pub struct Environment {
     pub symbols: HashMap<String, LiteralType>,
-    pub parent: Option<Rc<Environment>>
+    pub parent: Option<rc::Rc<RefCell<Environment>>>
 }
 
 impl<'a> Environment {
-    pub fn new_global() -> Rc<Environment> {
-        Rc::new(Environment { symbols: HashMap::new(), parent: None })
+    pub fn new_global() -> rc::Rc<RefCell<Environment>> {
+        rc::Rc::new(RefCell::new(Environment { 
+            symbols: HashMap::new(), parent: None }))
     }
 
-    pub fn new_frame(par: &mut Rc<Environment>) -> Rc<Environment> {
-        Rc::new(Environment { symbols: HashMap::new(), parent: Some(par.make_unique()) })
+    pub fn new_frame(par: rc::Rc<RefCell<Environment>>) -> rc::Rc<RefCell<Environment>> {
+        rc::Rc::new(RefCell::new(Environment { 
+            symbols: HashMap::new(), parent: Some(par) }))
     }
 
     pub fn lookup(&'a self, var: &String) -> CalcResult<&'a LiteralType> {
         match self.symbols.find(var) {
             Some(ref val) => Ok(*val),
             None      => match self.parent {
-                Some(ref par) => par.lookup(var),
+                Some(ref par) => par.borrow().lookup(var),
                 None => Err(UnboundArg(var.clone()))
             }
         }
     }
 }
+/*
+pub trait Lookup<'a, T> {
+    fn lookup(&'a self, var: &String) -> CalcResult<&'a T>;
+}
 
+impl<'a> Lookup<'a, LiteralType> for rc::Rc<RefCell<Environment>> {
+    pub fn lookup(&'a self, var: &String) -> CalcResult<&'a LiteralType> {
+        match *self.symbols.find(var) {
+            Some(ref val) => Ok(*val),
+            None      => match self.parent {
+                Some(ref par) => par.lookup(var),
+                None => Err(UnboundArg(var.clone()))
+            }
+        }
+        self.loo
+    }
+}
+*/
 impl fmt::Show for Environment {
     #[inline]
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
